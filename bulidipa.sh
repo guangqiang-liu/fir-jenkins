9e6baf35dbc40aa24d3594f52f7e5077bdcad7f9
guangqiang <1126756952@qq.com>
2016-08-22 17:40:57 +0800

    sub

---
+++ bulidipa.sh
@@ -0,0 +1,6 @@
+
+export LC_ALL="en_US.UTF-8"
+fir_bulid
+projectScheme="LSBuyerDev"
+current_State="Release"
+fir build_ipa ${WORKSPACE}/$projectScheme.xcworkspace  -w -S $projectScheme -C $current_State -p -V
